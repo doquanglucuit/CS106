d144e83ad96e10831f17bf545e4f732670c88e89
doquanglucuit <23520902@gm.uit.edu.vn>
2025-03-16 14:59:08 +0700

    Write bfs function

--- BT1 - DFS-BFS-UCS for Sokoban/solver.py
+++ BT1 - DFS-BFS-UCS for Sokoban/solver.py
@@ -157,7 +157,7 @@ def depthFirstSearch(gameState):
     while frontier:
         node = frontier.pop()
         node_action = actions.pop()
-        if isEndState(node[-1][-1]):
+        if isEndState(node[-1][1]):
             temp += node_action[1:]
             break
         if node[-1] not in exploredSet:
@@ -181,6 +181,22 @@ def breadthFirstSearch(gameState):
     actions = collections.deque([[0]])
     temp = []
     ### CODING FROM HERE ###
+    while frontier:
+        node = frontier.popleft()
+        node_action = actions.popleft()
+        if isEndState(node[-1][1]):
+            temp += node_action[1:]
+            break
+        if node[-1] not in exploredSet:
+            exploredSet.add(node[-1])
+            for action in legalActions(node[-1][0], node[-1][1]):
+                newPosPlayer, newPosBox = updateState(node[-1][0], node[-1][1], action)
+                if isFailed(newPosBox):
+                    continue
+                frontier.append(node + [(newPosPlayer, newPosBox)])
+                actions.append(node_action + [action[-1]])
+    return temp
+    
 
 def cost(actions):
     """A cost function"""
@@ -199,6 +215,7 @@ def uniformCostSearch(gameState):
     actions.push([0], 0)
     temp = []
     ### CODING FROM HERE ###
+
 
 """Read command"""
 def readCommand(argv):
